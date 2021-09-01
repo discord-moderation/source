8598378434e64a93abb4ff3224a38e2fc5f016e9
bad-boy-discord <46648824+bad-boy-discord@users.noreply.github.com>
2021-09-01 17:23:17 +0300

    Delete index.js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    Moderation: require('./classes/Moderation'),
-    MuteHandler: require('./classes/MuteHandler'),
-    WarnHandler: require('./classes/WarnHandler'),
-    Logger: require('./classes/Logger'),
-    Utils: require('./classes/Utils')
-}
